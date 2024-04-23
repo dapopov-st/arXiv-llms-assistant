e4b98ff6edfcccc455bff40149ea4c6702a9ea29
dapopov-st <dpopovvelasco@gmail.com>
2024-04-23 08:53:15 -0400

    comment run.sh and make exit 0 description specific

--- run.sh
+++ run.sh
@@ -12,13 +12,15 @@ echo "Script dir: $script_dir"
 python scripts/get_80-20_articles.py --directory_unread_csv $latest_file --directory_read_txts ./abstracts
 
 # Loop over abstracts_new to get arXiv markup
+# Move processed files to abstracts_new_processed or abstracts_new_cant_process
+# Want to see progress at the command line, so doing this in bash instead of Python
 for file in abstracts_new/*.txt; do
     filename=$(basename -- "$file" .txt)
     echo "Processing $filename"
     python ./scripts/get_arxiv_markup.py --arxiv_abbrev $filename --write_dir ./markups_new
     exit_status=$?
     if [ $exit_status -eq 0 ]; then
-        echo "Success"
+        echo "Success processing $filename. Moving to abstracts_new_processed."
         mv "$file" "abstracts_new_processed/$filename.txt"
     else
         echo "Failed to process $filename, likely since Markup is not (yet?) available."
